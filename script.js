078f015f7ae036dedde195e22dfd0b55d633ddb9
alexpredus00 <alexpredus@yahoo.com>
2022-09-13 02:57:57 +0300

    Finished version

--- script.js
+++ script.js
@@ -1,13 +1,15 @@
-let passOne = document.getElementById('password_one');
-let passTwo = document.getElementById('password_two');
 let passMatch = document.getElementById('password_match');
 
 $('input[type="submit"]').click( function(){
     $('form').addClass('submitted');
+    let passOne = document.getElementById('password_one').value;
+    let passTwo = document.getElementById('password_two').value;
+    console.log(passOne + "   " + passTwo);
     if(passOne == passTwo){
         passMatch.innerHTML = '';
     } else {
         passMatch.innerHTML = '* passwords do not match';
+        return false;
     }
 });
 
